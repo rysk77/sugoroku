9020d7ed53661878879d2d65a2c37c775e39173b
rysk77 <61385830+rysk77@users.noreply.github.com>
2020-03-17 09:20:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 
 var $id = function(id){ return document.getElementById(id); };
-var count = 1;
-var sum = 0;
-var dice;
+var countVal = 1;　//投げた数をカウント
+var leftVal = 100; //残りのマス
+var diceFig; //サイコロの図を決定
+var diceNum; //サイコロの価
 
 var rollDice = function(){
-	var diceNumber = Math.floor(Math.random() * 6) + 1;
-	dice = diceNumber + ".png";
-	$id('result').innerHTML = "<img src='" + dice + "' width='64' height='64'>"
-	count++;
-	$id('count').innerHTML = count + "投目";
-	sum = sum + diceNumber;
-	$id('position').innerHTML = "スタートから"+sum+"マス目";
+	 diceNum = Math.floor(Math.random() * 6) + 1;
+	 diceFig = diceNum + ".png";
+	 $id('result').innerHTML = "<img src='" + diceFig + "' width='64' height='64'>"
+	 countVal++;
+ 	 $id('count').innerHTML = countVal + "投目";
+	 leftVal = leftVal - diceNum;
+	 $id('position').innerHTML = "ゴールまで"+leftVal+"マス";
+	 if(leftVal<=0){
+		 $id('goal').innerHTML = "ゴールです！おめでとうございます"
+		 $id('position').innerHTML = "";
+		 $id('rollBtn').onclick = "";
+}
 }
